25091ebdc4a452bcf8ca61d91e2c622a9fee9c6e
Jorge Almonacid <jorge.almonacid@overactive.com>
2022-03-24 15:57:07 -0300

    Storybook

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,39 @@
+import './mylabel.css';
+
+interface Props {
+  /**
+   * Este es el mensaje a mostrar en la etiqueta
+   */
+  label: string;
+  /**
+   * Este es el tamaño de la etiqueta
+   */
+  size: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+   * Si quiere todo capitalizado
+   */
+  allCaps?: boolean;
+  /**
+   * Colores básicos del botón
+   */
+  color?: 'primary' | 'secondary' | 'tertiary'
+  /**
+   * Color personalizado de la fuente
+   */
+   fontColor?: string;
+}
+
+export const MyLabel = ({
+    allCaps = false,
+    color = 'primary',
+    label = 'No Label',
+    size = 'normal',
+    fontColor
+  }: Props) => {
+  return (
+    <span
+      className={`label ${ size } text-${color}`}
+      style={{ color: fontColor }}>
+        { allCaps ? label.toUpperCase() : label }</span>
+  )
+}
